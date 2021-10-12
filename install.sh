b972f9efe03baee87daeed6da3026ff8652e9988
Benj Ledesma <benj.ledesma@gmail.com>
2021-10-12 20:50:45 +0800

    Add optional installs & yarn dependencies

--- install.sh
+++ install.sh
@@ -17,3 +17,33 @@ for cfgdir in $(ls config); do
   [ -e "$cfgpath" ] && mv -v "$cfgpath" backup/config/
   ln -sfv "$BASE/config/$cfgdir" "$cfgpath"
 done
+
+## optional installs
+
+# install yarn dependencies
+if [[ " $@ " =~ " yarn " ]]; then
+  yarn global add $(cat dependencies/yarn-global.txt)
+fi
+
+# install vim deps
+if [[ " $@ " =~ " vim " ]]; then
+  # vim-plug (nvim)
+  sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+  # packer (nvim)
+  git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+     ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+fi
+
+# install tmux plugins
+if [[ " $@ " =~ " tmux " ]]; then
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
+
+
+# install fzf
+if [[ " $@ " =~ " fzf " ]]; then
+  git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+  ~/.fzf/install
+fi
